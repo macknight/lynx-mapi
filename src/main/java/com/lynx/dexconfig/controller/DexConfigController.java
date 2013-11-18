920e917c1140d2c769c1af515267ead417f09c13
zhufeng.liu <zhufeng.liu@dianping.com>
2013-11-18 20:06:38 +0800

    接口参数调整

--- src/main/java/com/lynx/dexconfig/controller/DexConfigController.java
+++ src/main/java/com/lynx/dexconfig/controller/DexConfigController.java
@@ -24,91 +24,91 @@ import java.util.List;
  */
 @Controller
 @RequestMapping(value = "/dex", method = RequestMethod.POST,
-        produces = "text/plain;charset=UTF-8")
+		produces = "text/plain;charset=UTF-8")
 public class DexConfigController extends BasicController {
 
-    @Autowired
-    private DexConfigService dexConfigService;
+	@Autowired
+	private DexConfigService dexConfigService;
 
-    @Autowired
-    private PluginService pluginService;
+	@Autowired
+	private PluginService pluginService;
 
-    /**
-     * 获取框架service配置
-     *
-     * @param ua
-     * @param token
-     * @return
-     */
-    @RequestMapping("/service")
-    public ResponseEntity<String> frameworkConfig(@RequestParam(value = "ua", required = true) String ua,
-                                                  @RequestParam(value = "token", required = false) String token) {
-        Result result = null;
-        try {
-            List<Config> config = dexConfigService.getDexServiceConfig(ua, token);
-            if (config != null) {
-                result = new Result(Result.RS_OK, config);
-            } else {
-                result = new Result(Result.RS_FAIL, "cant get framework dexconfig");
-            }
-        } catch (Exception e) {
-            result = new Result(Result.RS_ERROR, "server inner error");
-        }
-        return new ResponseEntity<String>(EncryptUtil.format(result), HttpStatus.OK);
-    }
+	/**
+	 * 获取框架service配置
+	 *
+	 * @param ua
+	 * @param token
+	 * @return
+	 */
+	@RequestMapping("/service")
+	public ResponseEntity<String> frameworkConfig(@RequestParam(value = "ua", required = true) String ua,
+	                                              @RequestParam(value = "token", required = false) String token) {
+		Result result = null;
+		try {
+			List<Config> config = dexConfigService.getDexServiceConfig(ua, token);
+			if (config != null) {
+				result = new Result(Result.RS_OK, config);
+			} else {
+				result = new Result(Result.RS_FAIL, "cant get framework dexconfig");
+			}
+		} catch (Exception e) {
+			result = new Result(Result.RS_ERROR, "server inner error");
+		}
+		return new ResponseEntity<String>(EncryptUtil.format(result), HttpStatus.OK);
+	}
 
-    /**
-     * 获取插件更新配置
-     *
-     * @return
-     */
-    @RequestMapping(value = "/myplugin", method = RequestMethod.POST,
-            produces = "text/plain;charset=UTF-8")
-    public ResponseEntity<String> myPlguins(@RequestParam(value = "categories", required = true) String categories) {
-        Result result = null;
-        try {
-            String[] tmp = categories.split("\\|");
-            List<String> cats = new ArrayList<String>();
-            for (String cat : tmp) {
-                cats.add(cat);
-            }
-            if (cats.size() <= 0) {
-                result = new Result(Result.RS_FAIL, "参数错误");
-            } else {
-                List<Plugin> plugins = pluginService.myPlugins(cats);
-                if (plugins != null && plugins.size() > 0) {
-                    result = new Result(Result.RS_OK, plugins);
-                } else {
-                    result = new Result(Result.RS_FAIL, "no dexconfig in store yet");
-                }
-            }
-        } catch (Exception e) {
-            log.info(e.getStackTrace());
-            result = new Result(Result.RS_ERROR, "server inner error");
-        }
-        return new ResponseEntity<String>(EncryptUtil.format(result), HttpStatus.OK);
-    }
+	/**
+	 * 获取插件更新配置
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "/myplugin", method = RequestMethod.POST,
+			produces = "text/plain;charset=UTF-8")
+	public ResponseEntity<String> myPlguins(@RequestParam(value = "apps", required = true) String data) {
+		Result result = null;
+		try {
+			String[] tmp = data.split("\\|");
+			List<String> apps = new ArrayList<String>();
+			for (String cat : tmp) {
+				apps.add(cat);
+			}
+			if (apps.size() <= 0) {
+				result = new Result(Result.RS_FAIL, "参数错误");
+			} else {
+				List<Plugin> plugins = pluginService.myPlugins(apps);
+				if (plugins != null && plugins.size() > 0) {
+					result = new Result(Result.RS_OK, plugins);
+				} else {
+					result = new Result(Result.RS_FAIL, "no dexconfig in store yet");
+				}
+			}
+		} catch (Exception e) {
+			log.info(e.getStackTrace());
+			result = new Result(Result.RS_ERROR, "server inner error");
+		}
+		return new ResponseEntity<String>(EncryptUtil.format(result), HttpStatus.OK);
+	}
 
-    /**
-     * 插件商店
-     *
-     * @return
-     */
-    @RequestMapping(value = "/pluginstore", method = RequestMethod.POST,
-            produces = "text/plain;charset=UTF-8")
-    public ResponseEntity<String> pluginsAtStore() {
-        Result result = null;
-        try {
-            List<Plugin> plugins = pluginService.allPlugins();
-            if (plugins != null && plugins.size() > 0) {
-                result = new Result(Result.RS_OK, plugins);
-            } else {
-                result = new Result(Result.RS_FAIL, "no dexconfig in store yet");
-            }
-        } catch (Exception e) {
-            log.info(e.getStackTrace());
-            result = new Result(Result.RS_ERROR, "server inner error");
-        }
-        return new ResponseEntity<String>(EncryptUtil.format(result), HttpStatus.OK);
-    }
+	/**
+	 * 插件商店
+	 *
+	 * @return
+	 */
+	@RequestMapping(value = "/pluginstore", method = RequestMethod.POST,
+			produces = "text/plain;charset=UTF-8")
+	public ResponseEntity<String> pluginsAtStore() {
+		Result result = null;
+		try {
+			List<Plugin> plugins = pluginService.allPlugins();
+			if (plugins != null && plugins.size() > 0) {
+				result = new Result(Result.RS_OK, plugins);
+			} else {
+				result = new Result(Result.RS_FAIL, "no dexconfig in store yet");
+			}
+		} catch (Exception e) {
+			log.info(e.getStackTrace());
+			result = new Result(Result.RS_ERROR, "server inner error");
+		}
+		return new ResponseEntity<String>(EncryptUtil.format(result), HttpStatus.OK);
+	}
 }
